6bade6a83f951c86ce1855e56b3c3f95df5e7c1c
Oussama <ma9nin.2000.dz@gmail.com>
2026-04-22 21:29:50 +0100

    fix case sensitive model import

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,7 +19,7 @@ module.exports = router;*/
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/User"); // ✅ مهم
+const User = require("../models/user"); // ✅ مهم
 
 const {
   registerUser,
